20d79ea2adf70649b80cb8326cba02358dcccc3c
Dariacz <51260648+Dariacz@users.noreply.github.com>
2020-02-23 19:53:40 +0100

    1st Commit base

---
+++ src/main/java/com/daria/project/quiz/StartupRunner.java
@@ -0,0 +1,40 @@
+package com.daria.project.quiz;
+
+import com.daria.project.quiz.database.entities.PlayerEntity;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Component
+@Log
+public class StartupRunner implements CommandLineRunner {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+        log.info("Executing startup actions...");
+
+        PlayerEntity player = new PlayerEntity("John");
+        log.info("Created player: " + player);
+
+        entityManager.persist(player);
+        log.info("Same player after persist: " + player);
+
+        log.info("List of players from database:");
+        Query q = entityManager.createQuery("SELECT p FROM PLAYERS p");
+        List<PlayerEntity> playersFromDb = (List<PlayerEntity>) q.getResultList();
+
+        for (PlayerEntity playerFromDb : playersFromDb) {
+            log.info("Player from DB: " + playerFromDb);
+        }
+    }
+}
--- src/main/java/com/daria/project/quiz/dto/HealthcheckDto.java
+++ src/main/java/com/daria/project/quiz/dto/HealthcheckDto.java
@@ -1,13 +1,11 @@
 package com.daria.project.quiz.dto;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 
-@Getter
 @AllArgsConstructor
-
+@Getter
 public class HealthcheckDto {
     private boolean status;
     private String message;

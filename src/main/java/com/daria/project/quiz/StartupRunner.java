94b44d05de9d7f376a4ba4eb40be057339548eab
Dariacz <51260648+Dariacz@users.noreply.github.com>
2020-02-25 18:57:56 +0100

    Add json categories

--- src/main/java/com/daria/project/quiz/StartupRunner.java
+++ src/main/java/com/daria/project/quiz/StartupRunner.java
@@ -1,6 +1,8 @@
 package com.daria.project.quiz;
 
 import com.daria.project.quiz.database.entities.PlayerEntity;
+import com.daria.project.quiz.database.repositories.PlayerRepository;
+import com.daria.project.quiz.services.QuizDataService;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -16,25 +18,25 @@ import java.util.List;
 public class StartupRunner implements CommandLineRunner {
 
     @Autowired
-    private EntityManager entityManager;
+    private PlayerRepository playerRepository;
+    @Autowired
+    private QuizDataService quizDataService;
 
     @Override
-    @Transactional
-    public void run(String... args) throws Exception {
+    public void run(String...args) throws Exception {
         log.info("Executing startup actions...");
-
-        PlayerEntity player = new PlayerEntity("John");
-        log.info("Created player: " + player);
-
-        entityManager.persist(player);
-        log.info("Same player after persist: " + player);
+        playerRepository.save(new PlayerEntity("John"));
+        playerRepository.save(new PlayerEntity("Harry"));
+        playerRepository.save(new PlayerEntity("George"));
 
         log.info("List of players from database:");
-        Query q = entityManager.createQuery("SELECT p FROM PLAYERS p");
-        List<PlayerEntity> playersFromDb = (List<PlayerEntity>) q.getResultList();
-
-        for (PlayerEntity playerFromDb : playersFromDb) {
-            log.info("Player from DB: " + playerFromDb);
+        List<PlayerEntity> playersFromDatabase = playerRepository.findAll();
+        for (PlayerEntity player : playersFromDatabase) {
+            log.info("Retrieved player: " + player);
         }
+
+        quizDataService.getQuizCategories();
     }
+
+
 }
